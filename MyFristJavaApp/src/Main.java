f3d0ff31852d8287519a0cabc677053c1d04360f
Moises VN <moisesvn.dev@gmail.com>
2025-05-08 11:18:08 -0300

    Criação de um menu interativo para acessar a função de outras classes.

--- MyFristJavaApp/src/Main.java
+++ MyFristJavaApp/src/Main.java
@@ -1,6 +1,51 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Variables.variaveis();
-        RepresentacaoNumerica.representacaoNumerica();
+        Scanner scanner = new Scanner(System.in);
+        int opcao;
+
+        do{
+            System.out.println("---------- Menu de opções -----------");
+            System.out.println("Escolha uma das funções abaixo: \n");
+            System.out.println("1 - Exibir a tabela das variaveis primitivas do Java");
+            System.out.println("2 - Exibir as representações numericas do Java");
+            System.out.println("3 - Calculadora de media de nota");
+            System.out.println("4 - Conversor de temperatura");
+            System.out.println("0 - para encerra o programa");
+            System.out.print("Escolha ma das opções acima: ");
+            opcao = scanner.nextInt();
+
+            switch (opcao){
+                case 1:
+                    Variables.variaveis();
+                    break;
+                case 2:
+                    RepresentacaoNumerica.representacaoNumerica();
+                    break;
+                case 3:
+                    MediaNotasAluno.menu();
+                    break;
+                case 4:
+                    ConversorTemperatura.menu();
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+                case 9:
+                    break;
+                case 0:
+                    System.out.println("Programa Encerrado...");
+                    break;
+                default:
+                    System.out.println("Opção digitada invalida");
+                    break;
+            }
+        }while (opcao!=0);
     }
 }
