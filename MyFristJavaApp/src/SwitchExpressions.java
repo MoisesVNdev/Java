6e0631440dfb46c842978821bcf0273afe6aba72
Moises VN <moisesvn.dev@gmail.com>
2025-05-09 10:13:34 -0300

    Criação da nova class SwitchExpressions que mostra a quantidade de dias de um mês ao passa  valor do numero correspondete do mês, exemplo: janeiro = 1 , dezembro = 12.

---
+++ MyFristJavaApp/src/SwitchExpressions.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class SwitchExpressions {
+    private static Scanner scanner = new Scanner(System.in);
+    private static int dias, mes;
+    public static void menu(){
+
+        do
+        {
+            System.out.println("\nDigite um numero de 1 a 12 para saber quantos dias tem nesse mês!");
+            System.out.print("Resposta: ");
+            diasNoMes(mes = scanner.nextInt());
+
+            System.out.println(dias + " Dias");
+            System.out.println("\n1 - para tentar novamente ");
+            System.out.println("0 - para voltar ao menu anterior");
+            System.out.print("Resposta: ");
+            mes= scanner.nextInt();
+        }while (mes!=0);
+    }
+    public static int diasNoMes(int mes){
+        dias = switch (mes){
+            case 1, 3, 5, 7, 8, 10, 12 -> 31;
+            case 2 -> 28;
+            case 4, 6, 9, 11 -> 30;
+            default -> {
+                System.out.println("Valor invalido");
+                yield 0;
+            }
+        };
+        return dias;
+    }
+}
